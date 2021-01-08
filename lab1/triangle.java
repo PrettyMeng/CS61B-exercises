b9aad45669afafb978e029eae88be31b2a7db094
PrettyMeng <zhoumeng9904@sjtu.edu.cn>
2021-01-08 16:15:32 +0800

    hw0 exercise

---
+++ lab1/triangle.java
@@ -0,0 +1,11 @@
+public class triangle {
+    public static void main(String[] args){
+        int i = 1;
+        String a = "*";
+        while (i < 6){
+            System.out.println(a);
+            a += "*";
+            i += 1;
+        }
+    }
+}
