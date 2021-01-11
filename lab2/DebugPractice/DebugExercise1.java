5324de340dc99c583d69f9e41cb8af0fa9af0db1
PrettyMeng <zhoumeng9904@sjtu.edu.cn>
2021-01-11 11:33:33 +0800

    part of lab2

--- lab2/DebugPractice/DebugExercise1.java
+++ lab2/DebugPractice/DebugExercise1.java
@@ -20,7 +20,7 @@ public class DebugExercise1 {
         int result2 = divideThenRound(t2, b2);
         System.out.println("round(" + t2 + "/" + b2 + ")=" + result2);
 
-        int t3 = 3;
+          int t3 = 3;
         int b3 = 4;
         int result3 = divideThenRound(t3, b3);
         System.out.println("round(" + t3 + "/" + b3 + ")=" + result3);
