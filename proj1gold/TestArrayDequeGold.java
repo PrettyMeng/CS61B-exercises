018352554b33dc7fb8a1a8d7d63918cd98d495ef
PrettyMeng <zhoumeng9904@sjtu.edu.cn>
2021-03-30 20:57:12 +0800

    initial implementation

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class TestArrayDequeGold {
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestArrayDequeGold {
+    @Test
+    public void testStudentArrayDeque() {
+        ArrayDequeSolution<Integer> solutionDeque = new ArrayDequeSolution<>();
+        StudentArrayDeque<Integer> studentDeque = new StudentArrayDeque<>();
+        String debuggingInfo = "";
+        for (int i = 0; i < 10; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+
+            if (numberBetweenZeroAndOne < 0.5) {
+                debuggingInfo += String.format("\naddLast(%d)", i);
+                solutionDeque.addLast(i);
+                studentDeque.addLast(i);
+            } else {
+                debuggingInfo += String.format("\naddFirst(%d)", i);
+                solutionDeque.addFirst(i);
+                studentDeque.addFirst(i);
+            }
+        }
+
+        for (int i = 0; i < 10; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            if (numberBetweenZeroAndOne < 0.5) {
+                debuggingInfo += String.format("\nremoveLast(%d)", i);
+                assertEquals(debuggingInfo, solutionDeque.removeLast(), studentDeque.removeLast());
+            } else {
+                debuggingInfo += String.format("\nremoveFirst(%d)", i);
+                assertEquals(debuggingInfo, solutionDeque.removeFirst(), studentDeque.removeFirst());
+            }
+
+        }
+    }
 }
