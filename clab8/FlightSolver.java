e9e7689db1bcf2bc1142c0180f192fb2f30c3cd9
PrettyMeng <zhoumeng9904@sjtu.edu.cn>
2021-08-24 18:17:11 +0800

    Flight problem solved

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,7 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -8,13 +11,43 @@ import java.util.ArrayList;
  */
 public class FlightSolver {
 
+    private Comparator<Flight> startComparator = (flight1, flight2) -> flight1.startTime - flight2.startTime;
+    private Comparator<Flight> endComparator = (flight1, flight2) -> flight1.endTime - flight2.endTime;
+    private PriorityQueue<Flight> start_queue;
+    private PriorityQueue<Flight> end_queue;
+
+
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        start_queue = new PriorityQueue<>(startComparator);
+        end_queue = new PriorityQueue<>(endComparator);
+        for (Flight flight : flights) {
+            start_queue.add(flight);
+            end_queue.add(flight);
+        }
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        int cur_passengers = 0, max_passengers = -1;
+        // no need to check end_queue, since the landning
+        // will just decrease the number of passengers.
+        while (start_queue.peek() != null) {
+            Flight takeOffFlight = start_queue.peek();
+            Flight landingFlight = end_queue.peek();
+            int min_start = takeOffFlight.startTime;
+            int min_end = landingFlight.endTime;
+            if (min_start < min_end) {
+                cur_passengers += takeOffFlight.passengers;
+                if (cur_passengers > max_passengers) {
+                    max_passengers = cur_passengers;
+                }
+                start_queue.remove(takeOffFlight);
+            } else {
+                cur_passengers -= landingFlight.passengers;
+                end_queue.remove(landingFlight);
+            }
+        }
+
+        return max_passengers;
     }
 
 }
